aea9b7449fa2f4aaa81e7c28d59dcdba8e39933b
TheOneJamJam - cherij2 <112198378+cherij2@users.noreply.github.com>
2025-10-07 21:05:54 -0400

    some stuff i forgot to add in prev comit

--- coe4dk4_lab_2_2025/main.c
+++ coe4dk4_lab_2_2025/main.c
@@ -108,6 +108,7 @@ main(void)
 
     data.buffer = fifoqueue_new();
     data.link   = server_new();
+    data.link2   = server_new();
 
     /* 
      * Set the random number generator seed for this run.
